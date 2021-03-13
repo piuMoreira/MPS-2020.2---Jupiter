c8451bb001edea9b24b69dc77700a73b1bf50d0f
MatheusGSantos <mths.gm@gmail.com>
2021-03-12 21:12:39 -0300

    Split Arquivo_D to Arquivo_D_1

---
+++ src/Arquivo_D_1.java
@@ -0,0 +1,16 @@
+public class Arquivo_D_1 {
+	private String message;
+
+	public Arquivo_D_1() {
+		this.message = "Este é o arquivo D";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
