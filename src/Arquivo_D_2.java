c752c799d6bad04300c7aee4add0e9bf4e62badf
MatheusGSantos <mths.gm@gmail.com>
2021-03-12 21:49:29 -0300

    Split Arquivo_D to Arquivo_D_2

---
+++ src/Arquivo_D_2.java
@@ -0,0 +1,11 @@
+public class Arquivo_D_2 {
+	private String related;
+
+	public String getRelated() {
+		return related;
+	}
+
+	public void setRelated(String related) {
+		this.related = related;
+	}
+}
